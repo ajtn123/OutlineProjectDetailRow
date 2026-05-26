b3aece2b14d2283ff1593544f24fbefa2873fda4
ajtn123 <Ajitani-Hifumi@outlook.com>
2026-05-26 13:38:57 +0800

    update

--- DuplicateCheckerConsole/Program.cs
+++ DuplicateCheckerConsole/Program.cs
@@ -11,28 +11,12 @@ Console.Write("Started. ");
 Checker.Check();
 
 Console.WriteLine($"Finished. Found {Checker.Duplicates.Count} Matches.");
-Console.WriteLine();
 
 foreach (var item in Checker.Duplicates)
 {
-    Console.WriteLine(BitConverter.ToString(item.Hash));
+    Console.WriteLine();
+    Console.WriteLine(Convert.ToHexString(item.Hash));
 
     foreach (var i in item.Files)
-        Console.WriteLine($" > {i.FullName}");
-
-    Console.WriteLine();
+        Console.WriteLine($" - {i.FullName}");
 }
-
-Console.Write("Save result? (Y/n) > ");
-if (Console.ReadLine() is not "n" and not "N")
-{
-    var dupInfo = Checker.Duplicates.Select(d => new DuplicateSetInfo(BitConverter.ToString(d.Hash), [.. d.Files.Select(f => f.FullName)]));
-    string json = System.Text.Json.JsonSerializer.Serialize(dupInfo, new System.Text.Json.JsonSerializerOptions()
-    {
-        Encoder = System.Text.Encodings.Web.JavaScriptEncoder.Create(System.Text.Unicode.UnicodeRanges.All),
-        WriteIndented = true
-    });
-    File.WriteAllText($"Result-{DateTime.Now.Millisecond}.json", json);
-}
-
-record DuplicateSetInfo(string Hash, string[] Files);
