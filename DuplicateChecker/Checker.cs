c89108aa677b2214e496314003c24a4f126717a9
aoi <Ajitani-Hifumi@outlook.com>
2026-06-10 02:43:48 +0800

    Refactor Checker class for improved file handling

--- DuplicateChecker/Checker.cs
+++ DuplicateChecker/Checker.cs
@@ -5,16 +5,14 @@ namespace DuplicateChecker;
 
 public class Checker
 {
-    public Checker(string[] paths, bool recursive = true, string searchPattern = "")
+    public Checker(string[] paths, bool recursive = true, string pattern = "")
     {
-        SearchPattern = searchPattern;
+        SearchPattern = pattern;
         Recursive = recursive;
-        Paths = [.. paths.Select(s => s.Trim('"', ' ', '\'')).Where(s => !string.IsNullOrWhiteSpace(s))];
-        Dirs = [.. Paths.Select(p => new DirectoryInfo(p))];
+        Paths = [.. paths.Select(s => s.Trim('"', ' ', '\'')).Where(Directory.Exists)];
     }
 
     public string[] Paths { get; }
-    public DirectoryInfo[] Dirs { get; }
     public bool Recursive { get; }
     public string SearchPattern { get; }
 
@@ -22,13 +20,13 @@ public class Checker
 
     public void Check()
     {
-        var files = Dirs.SelectMany(d => d.EnumerateFiles(SearchPattern, Recursive ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly))
-            .Where(f => (f.Attributes & FileAttributes.System) == 0)
-            .Distinct(new FileInfoEqualityComparer());
+        var files = Paths.SelectMany(path => Directory.EnumerateFiles(path, SearchPattern, new EnumerationOptions
+        {
+            RecurseSubdirectories = Recursive,
+            IgnoreInaccessible = true,
+        })).Distinct().Select(fp => new FileInfo(fp));
 
-        var lengthGroups = files.GroupBy(f => f.Length);
-
-        List<DuplicateSet> result = [];
+        var lengthGroups = files.GroupBy(file => file.Length);
 
         foreach (var lengthGroup in lengthGroups)
         {
@@ -40,7 +38,7 @@ public class Checker
             {
                 if (hashGroup.Count() == 1) continue;
 
-                result.Add(new(hashGroup.Key, [..hashGroup]));
+                Duplicates.Add(new(hashGroup.Key, [..hashGroup]));
             }
         }
     }
@@ -69,16 +67,4 @@ public class HashEqualityComparer : IEqualityComparer<byte[]>
         hash.AddBytes(obj);
         return hash.ToHashCode();
     }
-}
-
-public class FileInfoEqualityComparer : IEqualityComparer<FileInfo>
-{
-    public bool Equals(FileInfo? x, FileInfo? y)
-    {
-        if (x == null || y == null) return false;
-        return string.Equals(x.FullName, y.FullName, StringComparison.OrdinalIgnoreCase);
-    }
-
-    public int GetHashCode([DisallowNull] FileInfo obj)
-        => StringComparer.OrdinalIgnoreCase.GetHashCode(obj.FullName);
 }
